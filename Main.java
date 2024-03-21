9a1c9944df0599f825222f94d74757413a5034e6
_TRUPPER_ <117923568+NeTRUPPER@users.noreply.github.com>
2024-03-21 22:47:30 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,36 @@
-class Andriyanov_b{
-    // Объявление класса Andriyanov_b.
-    public static void register_bukv(){ // Объявление статического метода register_bukv(), возвращающего целочисленное значение.
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char zadanie;
 
-        Scanner scanner = new Scanner(System.in); // Создание объекта Scanner для считывания ввода пользователя из консоли.
+        do {
+            System.out.println();
+            System.out.println();
+            System.out.print(
+                    "[a] Налитов Андрей |Подсчет количества гласных букв в строке|\n" +
+                            "[b] Андриянов Влад |Привидение всех букв к верхнему и нижнему регистру|\n" +
+                            "[c] Шумский Илья |Удаление всех знаков препинания и служебных символов (кроме пробела) из строки|\n\n" +
+                            "Введите букву задания или 'q' для выхода: "
+            );
+            zadanie = scanner.nextLine().charAt(0);
 
-        System.out.print("Введите строку: "); // Вывод текста для запроса ввода строки.
-        String input = scanner.nextLine(); // Считывание строки, введенной пользователем, и сохранение в переменную input.
+            switch (zadanie) {
+                case 'a':
+                    Nalitov_a.str_in_and_out();
+                    break;
+                case 'b':
+                    Andriyanov_b.register_bukv();
+                    break;
+                case 'c':
+                    Shumskiy_C.znaki_prep();
+                    break;
+                case 'q':
+                    System.out.println("Выход из программы.");
+                    break;
+                default:
+                    System.out.println("Неправильный выбор. Попробуйте еще раз.");
+            }
+        } while (zadanie != 'q');
 
-        String Verhniy = input.toUpperCase(); // Преобразование строки input в верхний регистр и сохранение в переменную Verhniy.
-        String Nigniy = input.toLowerCase(); // Преобразование строки input в нижний регистр и сохранение в переменную Nigniy.
-
-        System.out.print("Верхний регистр: " + Verhniy + "\n"); // Вывод строки верхнего регистра.
-        System.out.print("Нижний регистр: " + Nigniy); // Вывод строки нижнего регистра.
     }
 }

b4e02f8696c91043fac595afb8b4de19b17fdc7f
_TRUPPER_ <vlad.andriyand.2005@gmail.com>
2024-03-21 21:57:38 +0700

    First

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Andriyanov_b{
+    // Объявление класса Andriyanov_b.
+    static void register_bukv(){ // Объявление статического метода register_bukv(), возвращающего целочисленное значение.
+
+        Scanner scanner = new Scanner(System.in); // Создание объекта Scanner для считывания ввода пользователя из консоли.
+
+        System.out.print("Введите строку: "); // Вывод текста для запроса ввода строки.
+        String input = scanner.nextLine(); // Считывание строки, введенной пользователем, и сохранение в переменную input.
+
+        String Verhniy = input.toUpperCase(); // Преобразование строки input в верхний регистр и сохранение в переменную Verhniy.
+        String Nigniy = input.toLowerCase(); // Преобразование строки input в нижний регистр и сохранение в переменную Nigniy.
+
+        System.out.print("Верхний регистр: " + Verhniy + "\n"); // Вывод строки верхнего регистра.
+        System.out.print("Нижний регистр: " + Nigniy); // Вывод строки нижнего регистра.
+
+    }
+}
